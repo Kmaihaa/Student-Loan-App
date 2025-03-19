424eca92da191f9897447cdb662a3bd6ed55c4c9
msharma9d <maaneksharma@gmail.com>
2025-03-19 14:10:02 -0400

    updating users ability to edit/delete expenses, along with a dynamic graph that changes when updates are made

--- app/src/main/java/com/example/student_loan_app/ExpenseAdapter.java
+++ app/src/main/java/com/example/student_loan_app/ExpenseAdapter.java
@@ -3,6 +3,7 @@ package com.example.student_loan_app;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,34 +11,57 @@ import java.util.List;
 
 public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder> {
 
-    private List<Expense> expenseList;
+    public interface OnExpenseItemClickListener {
+        void onEditExpense(ExpenseItem expenseItem);
+        void onDeleteExpense(ExpenseItem expenseItem);
+    }
 
-    public ExpenseAdapter(List<Expense> expenseList) {
-        this.expenseList = expenseList;
+    private List<ExpenseItem> expenseItemList;
+    private OnExpenseItemClickListener listener;
+
+    public ExpenseAdapter(List<ExpenseItem> expenseItemList, OnExpenseItemClickListener listener) {
+        this.expenseItemList = expenseItemList;
+        this.listener = listener;
     }
 
     @NonNull
     @Override
     public ExpenseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_expense, parent, false);
-        return new ExpenseViewHolder(view);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_expense, parent, false);
+        return new ExpenseViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ExpenseViewHolder holder, int position) {
-        Expense expense = expenseList.get(position);
+        ExpenseItem expenseItem = expenseItemList.get(position);
+        Expense expense = expenseItem.getExpense();
         holder.textViewName.setText(expense.getName());
         holder.textViewAmount.setText("$" + String.format("%.2f", expense.getAmount()));
+
+        // On short click: edit
+        holder.itemView.setOnClickListener(v -> {
+            if (listener != null) {
+                listener.onEditExpense(expenseItem);
+            }
+        });
+
+        // On long click: delete
+        holder.itemView.setOnLongClickListener(v -> {
+            if (listener != null) {
+                listener.onDeleteExpense(expenseItem);
+            }
+            return true;
+        });
     }
 
     @Override
     public int getItemCount() {
-        return expenseList.size();
+        return expenseItemList.size();
     }
 
     public static class ExpenseViewHolder extends RecyclerView.ViewHolder {
         TextView textViewName, textViewAmount;
+        // Optionally, add a delete button here if desired
 
         public ExpenseViewHolder(@NonNull View itemView) {
             super(itemView);
